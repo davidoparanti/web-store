2190180e493608f93e4eda93cf13ab121b01c5d1
David Oparanti <david.oparanti@gmail.com>
2021-07-01 22:39:12 +0100

    Add route

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
     return (
         <div>
             <Header />
-              <h1>Home Page</h1>
             <Switch>
                 <Route exact path="/">
+                     <h1>Home Page</h1>
                     <Photos />
                 </Route>
                 <Route path="/cart">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {BroswerRoter as Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import App from "./App"
 
@@ -10,3 +10,5 @@ ReactDOM.render(
     </Router>,
     document.getElementById("root")
 )
+
+
