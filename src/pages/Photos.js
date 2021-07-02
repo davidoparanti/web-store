6f348fb8de878842fb6ead6d810a10848b69179b
David Oparanti <david.oparanti@gmail.com>
2021-07-02 09:38:20 +0100

    Implement isFavourite

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React, {useContext} from "react"
+
+import Image from "../components/Image"
+import {getClass} from "../utils"
+import {Context} from "../Context"
 
 function Photos() {
+    const {allPhotos} = useContext(Context)
+    
+    const imageList = allPhotos.map((photo, index) => (
+            <Image key={photo.id} img={photo} className={getClass(index)} />
+        ))
+        
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+            {imageList}
         </main>
     )
 }
